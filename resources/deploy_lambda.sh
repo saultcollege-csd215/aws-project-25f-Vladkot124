dfcb50f31f164f13b4c42d99b71370f24d8de78b
Vladyslav Andres <24017497@saultcollege.ca>
2025-12-12 16:02:06 +0000

    Fix EC2 and Lambda deploy workflows

--- resources/deploy_lambda.sh
+++ resources/deploy_lambda.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# --- Configuration ---
 LAMBDA_NAME="csd215-lambda"
 REGION="us-east-1"
 ROOT=.
@@ -7,23 +8,23 @@ SOURCE_DIR="$ROOT/app"
 BUILD_DIR="lambda_package"
 ZIP_FILE="lambda_function.zip"
 
-set -e
+set -e   # Stop on errors
 
 echo "--- Starting deployment for $LAMBDA_NAME ---"
 
 echo "[1/4] Cleaning up previous builds..."
-rm -rf "$BUILD_DIR" "$ZIP_FILE"
+rm -rf "$BUILD_DIR"
+rm -f "$ZIP_FILE"
 mkdir -p "$BUILD_DIR/app"
 
 echo "[2/4] Copying application files..."
-# handler file at ZIP root (because Handler is lambda_app.main)
-cp "$SOURCE_DIR/lambda_app.py" "$BUILD_DIR/"
-
-# app package code
-cp "$SOURCE_DIR/__init__.py" "$SOURCE_DIR/core.py" "$SOURCE_DIR/data.py" "$SOURCE_DIR/lambda_app.py" "$BUILD_DIR/app/"
+cp "$SOURCE_DIR/lambda_app.py" "$BUILD_DIR"
+cp "$SOURCE_DIR"/__init__.py "$SOURCE_DIR"/core.py "$SOURCE_DIR"/data.py "$BUILD_DIR/app"
 
 echo "[3/4] Packaging the Lambda function..."
-(cd "$BUILD_DIR" && zip -r9 "../$ZIP_FILE" ./* -x "*.git*" -x "*.DS_Store" > /dev/null)
+cd "$BUILD_DIR"
+zip -r9 "../$ZIP_FILE" ./* -x "*.git*" -x "*.DS_Store" > /dev/null
+cd ..
 
 echo "[4/4] Deploying to AWS Lambda..."
 aws lambda update-function-code \
